156874bff150f2bbce71efe2a184e8256931156b
J█████ <dev.nullinside@gmail.com>
2024-10-23 23:52:42 -0400

    Adding more unit tests

--- src/Nullinside.Api.TwitchBot.Tests/ChatRules/StreamViewersTests.cs
+++ src/Nullinside.Api.TwitchBot.Tests/ChatRules/StreamViewersTests.cs
@@ -16,6 +16,11 @@ public class StreamViewersTests : AChatRuleUnitTestBase<StreamViewers> {
   [Test]
   [TestCase("@jellynyeko dо уоu alrеady triеd strеamviewers  оrg? Real viewеrs, fire works! Тhеy arе now giving оut а frее рackagе for streamers оО")]
   [TestCase("@kygaming98 dо уоu аlready tried streаmviewers  оrg? Real viewers, firе works! Thеy arе now giving оut а freе package fоr streamers oО")]
+  [TestCase("@hitpointgame2go do уоu alrеаdy tried strеamviewers  оrg? Rеal viewers, firе works! Theу are nоw giving out a freе package fоr strеаmеrs oO")]
+  [TestCase("@jellynyeko do yоu аlrеаdу tried streamviеwers  оrg? Rеal viеwеrs, fire works! Тhеу аre nоw giving out а frее рaсkagе fоr strеamers oO")]
+  [TestCase("@kirbyplayinggames do уоu аlrеаdy triеd streаmviewers  оrg? Rеal viеwers, fire wоrks! They аre now giving оut a frее package fоr strеаmers oO")]
+  [TestCase("@kygaming98 dо you alrеady tried streаmviewers  оrg? Real viewers, firе works! Тheу arе now giving оut a frее расkаgе fоr streаmеrs oO")]
+  [TestCase("@subjectbulbasaur do уоu аlready triеd strеаmviewers  оrg? Real viewеrs, fire works! Thеy arе nоw giving out а frеe packаge for strеаmеrs оO")]
   public async Task TestKnownStrings(string badString) {
     var rule = new StreamViewers();
     var botProxy = new Mock<ITwitchApiProxy>();
