ba8b81a9a76b7669725e88999bf87678ee2d6dba
J█████ <dev.nullinside@gmail.com>
2025-02-09 15:57:36 -0500

    fix: Index out of range exception in Cheap Viewers rule
    Need to generalize this and clean up the code later but this at least
    fixes the immediate issue with the method.
    
    nullinside-development-group/nullinside-api-twitch-bot#63

--- src/Nullinside.Api.TwitchBot.Tests/ChatRules/DoghypeTests.cs
+++ src/Nullinside.Api.TwitchBot.Tests/ChatRules/DoghypeTests.cs
@@ -15,6 +15,7 @@ public class DogehypeTests : AChatRuleUnitTestBase<Dogehype> {
   /// </summary>
   /// <param name="badString">The string that should fail.</param>
   [Test]
+  [TestCase("Visit dogehype dot com today and effortlessly boost your Twitch rankings!  @opAzPMVt")]
   [TestCase("Visit dogehype dot com and watch your channel grow today!  @5MGxTnYl")]
   [TestCase("Visit dogehype dot com today and climb the Twitch rankings with ease! Whether you're just starting out or looking to take your stream to the next level, DogeHype has the tools you need to succeed.  @gqznceDC")]
   [TestCase("Visit dogehype .biz com today and climb the Twitch rankings with ease! Whether you're just starting out or looking to take your stream to the next level, DogeHype has the tools you need to succeed.  @Axxq7ntz")]
